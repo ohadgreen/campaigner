b8f646e0e3669b0bae539acb5e695ba2e87c4c22
ohadgreen <ohad.green@gmail.com>
2020-06-14 08:24:22 +0300

    first commit

---
+++ src/main/java/com/mabaya/campaigner/CampaignerApplication.java
@@ -0,0 +1,13 @@
+package com.mabaya.campaigner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampaignerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampaignerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mabaya/campaigner/CampaignerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mabaya.campaigner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampaignerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
